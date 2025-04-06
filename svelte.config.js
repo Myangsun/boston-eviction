e6aae973dbd570ff481fd6cdec94de26bf9bed7f
Mingyang Sun <107828058+Myangsun@users.noreply.github.com>
2025-04-05 22:46:55 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,23 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
+const dev = process.env.NODE_ENV === "development";
+
 const config = {
   kit: {
     adapter: adapter({
-      // default options are shown
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-      precompress: false
+      pages: "build",
+      assets: "build",
+      fallback: "index.html",
+      precompress: false,
     }),
     prerender: {
-      handleHttpError: 'warn'
-    }
-  }
+      handleHttpError: "warn",
+    },
+    paths: {
+      base: dev ? "" : "/boston-eviction",
+    },
+  },
 };
 
 export default config;
