2a74996b9153256e9243195d47597527e504a2d4
Mingyang Sun <107828058+Myangsun@users.noreply.github.com>
2025-04-28 01:13:02 -0400

    rebuild

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [sveltekit()],
   server: {
     fs: {
-      allow: ['..']
-    }
-  }
+      allow: [".."],
+    },
+    base: "./",
+    build: {
+      outDir: "docs", // dump the build right into docs/
+    },
+  },
 });
